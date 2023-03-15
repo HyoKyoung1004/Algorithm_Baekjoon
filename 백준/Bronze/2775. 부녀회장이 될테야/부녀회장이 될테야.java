32fc9166db7be71166a5f9c68fa9a0997f82099b
HyoKyoung Lee <90086799+HyoKyoung1004@users.noreply.github.com>
2023-03-16 00:03:20 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 136 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        int[][] arr= new int[15][15];
+
+        for(int i=0;i<15;i++){
+            arr[0][i] = i;
+        }
+
+        for(int i=1;i<15;i++){
+            for(int j=1;j<15;j++){
+                arr[i][j] = arr[i-1][j] + arr[i][j-1];
+            }
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i<t;i++){
+            int k = Integer.parseInt(br.readLine());
+            int n=  Integer.parseInt(br.readLine());
+            sb.append(arr[k][n]+"\n");
+        }
+        System.out.println(sb);
+
+    }
+
+}
