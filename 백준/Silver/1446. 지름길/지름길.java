fc6d75738bc27e265e3b768400f8eb12a67fd31f
HyoKyoung Lee <90086799+HyoKyoung1004@users.noreply.github.com>
2023-03-15 23:57:49 +0900

    [Silver I] Title: 지름길, Time: 216 ms, Memory: 19352 KB -BaekjoonHub

---
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	static ArrayList<ArrayList<Node>> al;
+	static ArrayList<ArrayList<Node>> endAl;
+
+	static int N;
+	static int D;
+	static int[] distance;
+	static int[] startIndex;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		N = scan.nextInt();
+		D = scan.nextInt();
+
+		distance = new int[D + 1];
+		startIndex = new int[N];
+
+		al = new ArrayList<>(D + 1);
+		endAl = new ArrayList<>(D + 1);
+
+		for (int i = 0; i <= D; i++) {
+			al.add(new ArrayList<Node>());
+			endAl.add(new ArrayList<Node>());
+		}
+
+		for (int i = 0; i < N; i++) {
+
+			int s = scan.nextInt();
+			int e = scan.nextInt();
+			int d = scan.nextInt();
+
+			if (!(s >= 0 && s <= D && e >= 0 && e <= D))
+				continue;
+
+			al.get(s).add(new Node(e, d));
+			startIndex[i] = s;
+
+			if (e <= D)
+				endAl.get(e).add(new Node(s, d));
+		}
+
+		for (int i = 0; i <= D; i++) {
+			distance[i] = i;
+		}
+
+		for (int i = 0; i < N; i++) {
+			int s = startIndex[i];
+
+			for (Node tmp : al.get(s)) {
+
+				int e = tmp.getIndex();
+				int d = tmp.getDistance();
+
+
+				distance[e] = Math.min(distance[e], distance[s] + d);
+
+				for (int k = e + 1; k <= D; k++) {
+					distance[k] = Math.min(distance[k - 1] + 1, distance[k]);
+
+					for (Node endtmp : endAl.get(k))
+						distance[k] = Math.min(distance[k], distance[endtmp.getIndex()] + endtmp.getDistance());
+
+				}
+
+			}
+		}
+
+//		System.out.println("10: "+distance[10]);
+//		System.out.println("50: "+distance[50]);
+//		System.out.println("70: "+distance[70]);
+//		System.out.println("140: "+distance[140]);
+//		System.out.println("160: "+distance[160]);
+//		System.out.println("180: "+distance[180]);
+
+		System.out.println(distance[D]);
+
+	}
+
+}
+
+class Node {
+	int index;
+	int distance;
+
+	Node(int i, int d) {
+		index = i;
+		distance = d;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+}
