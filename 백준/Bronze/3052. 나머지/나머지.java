ca0c3943c3377e87ecf27e778906826361a2d759
HyoKyoung Lee <90086799+HyoKyoung1004@users.noreply.github.com>
2023-03-16 00:04:13 +0900

    [Bronze II] Title: 나머지, Time: 208 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		Set<Integer> set = new HashSet<>();
+		for(int i=0;i<10;i++) {
+			set.add(scan.nextInt()%42);
+		}
+		
+		System.out.println(set.size());
+	}
+
+}
