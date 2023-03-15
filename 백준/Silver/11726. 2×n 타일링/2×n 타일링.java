b782299dce645bba392d8709f7ab30679aef11e8
HyoKyoung Lee <90086799+HyoKyoung1004@users.noreply.github.com>
2023-03-16 00:08:45 +0900

    [Silver III] Title: 2×n 타일링, Time: 212 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan  = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        long[] D = new long[1001];
+
+        D[1]=1;
+        D[2] = 2;
+
+        long mod = 10007;
+        for(int i=3;i<=n;i++){
+            D[i] =(D[i-1]+D[i-2])%mod;
+        }
+
+        System.out.println(D[n]);
+    }
+}
