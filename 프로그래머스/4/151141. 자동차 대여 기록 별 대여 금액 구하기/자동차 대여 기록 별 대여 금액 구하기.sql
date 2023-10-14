53b3a07d575962b1eb129ea28e254bdc2f99f5f8
HyoKyoung Lee <90086799+HyoKyoung1004@users.noreply.github.com>
2023-10-14 21:39:27 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -0,0 +1,29 @@
+-- 코드를 입력하세요
+
+
+# 자동차 종류가 트럭 자동차의 대여기록
+# 대역 기록별로 대여 금액
+# 대여 기록 ID, 금액리스트 출력
+
+
+select HISTORY_ID,
+
+Case 
+when days>=90 then floor(daily_fee *days* (100 - (select discount_rate from CAR_RENTAL_COMPANY_DISCOUNT_PLAN where CAR_TYPE = '트럭' and  DURATION_TYPE='90일 이상'))/100 )
+when days>=30 then floor(daily_fee *days* (100 - (select discount_rate from CAR_RENTAL_COMPANY_DISCOUNT_PLAN where CAR_TYPE = '트럭' and  DURATION_TYPE='30일 이상'))/100 )
+when days>=7 then floor(daily_fee *days* (100 - (select discount_rate from CAR_RENTAL_COMPANY_DISCOUNT_PLAN where CAR_TYPE = '트럭' and  DURATION_TYPE='7일 이상'))/100 )
+else daily_fee *days
+end FEE
+
+from (
+select history_id, daily_fee, end_date, start_Date , to_days(end_date) - to_days(start_date)+1 days from CAR_RENTAL_COMPANY_CAR car
+join CAR_RENTAL_COMPANY_RENTAL_HISTORY history
+on car.CAR_ID = history.CAR_ID
+where CAR_TYPE ='트럭'
+) a
+order by fee desc, history_id desc
+
+# select history_id, daily_fee, end_date, start_Date , to_days(end_date) - to_days(start_date)+1 days from CAR_RENTAL_COMPANY_CAR car
+# join CAR_RENTAL_COMPANY_RENTAL_HISTORY history
+# on car.CAR_ID = history.CAR_ID
+# where CAR_TYPE ='트럭'
