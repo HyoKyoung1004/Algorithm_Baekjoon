1dbc62947c3368f3701cc2b426a5548bf87a27bf
HyoKyoung Lee <90086799+HyoKyoung1004@users.noreply.github.com>
2023-12-07 19:09:53 +0900

    [level 3] Title: 네트워크, Time: 0.22 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    
+    static boolean[] visit;
+    static int N;
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        N=n;
+        visit = new boolean[n];
+        for(int i=0;i<n;i++){
+            
+            if(visit[i]) continue;
+            bfs(i, computers);
+            answer++;
+            
+        }    
+        
+        return answer;
+    }
+    
+    private void bfs(int k, int[][] arr){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(k);
+        visit[k]=true;
+
+        while(!q.isEmpty()){
+            // System.out.println(q);
+            int now = q.poll();
+            for(int i=0;i<N;i++){
+
+                if(arr[now][i]==0 || visit[i] ) continue;
+
+                visit[i]=true;
+                q.add(i);
+            }
+
+        }
+    }
+}
