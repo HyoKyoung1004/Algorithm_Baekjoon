881a345db129789de33de8df69ee04f9f2df6400
HyoKyoung Lee <90086799+HyoKyoung1004@users.noreply.github.com>
2023-12-07 20:45:22 +0900

    [level 1] Title: 조건에 맞는 회원수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
+++ 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
@@ -2,5 +2,4 @@
 SELECT count(*) USERS
 from USER_INFO 
 # where JOINED like "2021%" and AGE between 20 and 29
-WHERE JOINED 
-BETWEEN '2021-01-01' AND '2021-12-31' and AGE between 20 and 29
+WHERE JOINED  BETWEEN '2021-01-01' AND '2021-12-31' and AGE between 20 and 29
