58ca6edf1946fb14862ed1c622c3bc96444157fe
HyoKyoung Lee <90086799+HyoKyoung1004@users.noreply.github.com>
2024-01-23 16:16:29 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -3,18 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        double last = Math.sqrt(n);        
-        // System.out.println(last);
+        int last = (int)Math.sqrt(n);        
         
-        for(int i=1;i<=n;i++){
-            if(n%i==0){
-                // System.out.println(i+", "+n/i);
+        for(int i=1;i<=last;i++){
+            if(n%i==0)
                 answer++;
-            }
-        
           }
-        // answer *=2;
-        // if(last*last == n) answer--;
+        answer *=2;
+        if(last*last == n) answer--;
         return answer;
     }
 }
