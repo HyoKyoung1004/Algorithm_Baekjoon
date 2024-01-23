892fa90b7412c650431e35396f7123952522b7d2
HyoKyoung Lee <90086799+HyoKyoung1004@users.noreply.github.com>
2024-01-23 16:10:43 +0900

    [Gold V] Title: 강의실 배정, Time: 672 ms, Memory: 69376 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -22,69 +22,20 @@ public class Main {
             arr[n][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-                    @Override
-                    public int compare(int[] o1, int[] o2) {
+        Arrays.sort(arr,  (o1,o2) -> {
                         if(o1[0] ==o2[0]) return o1[1] -o2[1];
                         return o1[0] -o2[0];
                     }
-                }
         );
 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); //minHeap
         for(int n=0;n<N;n++){
-            search(arr[n][0],arr[n][1]);
-//            System.out.println(list);
+            pq.add(arr[n][1]);
+            if(!pq.isEmpty() &&pq.peek() <= arr[n][0])
+                pq.poll();
         }
-        System.out.println(list.size());
+        System.out.println(pq.size());
 
     }
-    static void search(int s, int e){
-
-        int left = 0;
-        int right = list.size()-1;
-        int maxIdx =-1;
-        while(left<=right){
-
-            int mid =(left+right)/2;
-            if(list.get(mid) <=s){
-                left = mid+1;
-                maxIdx = mid;
-            }else{
-                right=mid-1;
-            }
-        }
-        //넣을 자리가 없음, 제일 뒤에 넣어
-        if(maxIdx==-1){
-//            list.add(e);
-        }else{
-            list.remove(maxIdx);
-//            list.set(maxIdx,e);
-//            Collections.sort(list);
-        }
-        searchIndex(e);
-
-    }
-    static void searchIndex(int target){
-        int left = 0;
-        int right = list.size()-1;
-        int maxIdx =-1;
-        while(left<=right){
-
-            int mid =(left+right)/2;
-            if(list.get(mid) <=target){
-                left = mid+1;
-                maxIdx = mid;
-            }else{
-                right=mid-1;
-            }
-        }
-        if(maxIdx==-1){
-            list.add(0,target);
-        }else{
-            list.add(maxIdx+1, target);
-        }
-//        System.out.println(list);
-    }
-
 
 }
