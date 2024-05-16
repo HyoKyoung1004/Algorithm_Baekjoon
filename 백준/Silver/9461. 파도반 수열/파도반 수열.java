a6261df8483889f1fdf81aa240964412fc056d5f
HyoKyoung Lee <90086799+HyoKyoung1004@users.noreply.github.com>
2024-05-16 18:52:57 +0900

    [Silver III] Title: 파도반 수열, Time: 136 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,21 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
 
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb=new StringBuffer();
+		
+		int tc = Integer.parseInt(br.readLine());
+		
+		
 		long[] arr = new long[101];
-		arr[1]=arr[2]=arr[3] = 1;
+		arr[1]=1;
+		arr[2]=1;
+		arr[3]=1;
 		
 		for(int i=4;i<=100;i++) {
-			arr[i] = arr[i-2]+arr[i-3];
+			arr[i] = arr[i-3]+arr[i-2];
 		}
 		
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		for(int i=0;i<n;i++) {
-			System.out.println(arr[scan.nextInt()]);
+		
+		
+		for(int t=0;t<tc;t++) {
+			
+			int n= Integer.parseInt(br.readLine());
+			sb.append(arr[n]+"\n");
+			
 		}
+		//System.out.println(arr[100]);
+		
+		sb.delete(sb.length()-1, sb.length());
+		System.out.println(sb);
 	}
 
 }
